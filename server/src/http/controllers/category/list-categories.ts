92f239d85429e45455dba996dcb8f6162accfa0b
Italo <italodopeey@gmail.com>
2023-11-08 12:12:07 -0300

    refactor: adding a promise

--- server/src/http/controllers/category/list-categories.ts
+++ server/src/http/controllers/category/list-categories.ts
@@ -1,10 +1,7 @@
 import { Request, Response } from 'express'
 import { ListCategoriesUseCase } from '../../../use-cases/categories/list-categories'
 
-export async function listCategoriesController(
-  request: Request,
-  reply: Response,
-) {
+export async function listCategoriesController(_: Request, reply: Response) {
   const listCategoriesUseCase = new ListCategoriesUseCase()
   const categories = await listCategoriesUseCase.handle()
 
--- server/src/use-cases/categories/list-categories.ts
+++ server/src/use-cases/categories/list-categories.ts
@@ -1,7 +1,17 @@
+import { Document } from 'mongoose'
 import { category } from '../../models/category'
 
+interface ListCategoriesUseCaseRequest {
+  name: string
+  icon: string
+}
+
+interface ListCategoriesUseCaseResponse
+  extends Document,
+    ListCategoriesUseCaseRequest {}
+
 export class ListCategoriesUseCase {
-  async handle() {
+  async handle(): Promise<ListCategoriesUseCaseResponse[]> {
     const categories = await category.find()
 
     return categories
