cb34f6b526efd1faf2b9fc4a028f6eb627aa73c6
Italo <italodopeey@gmail.com>
2023-11-09 01:48:37 -0300

    feat: controller listing products

--- server/src/http/controllers/product/list-product.ts
+++ server/src/http/controllers/product/list-product.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from 'express'
+import { ListProductsUseCase } from '../../../use-cases/product/list-products'
 
-export async function listProductController(request: Request, reply: Response) {
-  reply.send('Ok')
+export async function listProductsController(
+  request: Request,
+  reply: Response,
+) {
+  const listProductsUseCase = new ListProductsUseCase()
+  const products = await listProductsUseCase.handle()
+
+  reply.json(products)
 }
