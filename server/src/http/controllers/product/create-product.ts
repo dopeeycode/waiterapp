fd7f16e0186cbbd4c8de28393dbd6e88a67f3b33
Italo <italodopeey@gmail.com>
2023-11-10 21:31:32 -0300

    feat: ensuring than controller communicating with use case to creation product

--- server/src/http/controllers/product/create-product.ts
+++ server/src/http/controllers/product/create-product.ts
@@ -1,8 +1,33 @@
 import { Request, Response } from 'express'
+import { z } from 'zod'
+import { CreateProductUseCase } from '../../../use-cases/product/create-product'
 
 export async function createProductController(
   request: Request,
   reply: Response,
 ) {
-  reply.send('Ok')
+  const imagePath = request.file?.filename
+  const createProductBodySchema = z.object({
+    name: z.string(),
+    description: z.string(),
+    price: z.coerce.number(),
+    category: z.string(),
+    ingredients: z.string(),
+  })
+
+  const { name, description, category, ingredients, price } =
+    createProductBodySchema.parse(request.body)
+
+  const createProductUseCase = new CreateProductUseCase()
+
+  const product = await createProductUseCase.handle({
+    name,
+    description,
+    category,
+    ingredients,
+    price,
+    imagePath,
+  })
+
+  return reply.status(201).send(product)
 }
