7e5297548481a3a56702213ce474a0eec24855d9
Italo <italodopeey@gmail.com>
2023-11-10 21:29:20 -0300

    feat: use case to create product

---
+++ server/src/use-cases/product/create-product.ts
@@ -0,0 +1,31 @@
+import { Product } from '../../@types/Product'
+import { product } from '../../models/product'
+
+type CreateProductUseCaseRequest = Product
+
+export class CreateProductUseCase {
+  async handle({
+    name,
+    ingredients,
+    description,
+    category,
+    price,
+    imagePath,
+  }: CreateProductUseCaseRequest) {
+    try {
+      const productCreated = await product.create({
+        name,
+        description,
+        price: Number(price),
+        ingredients: JSON.parse(ingredients),
+        category,
+        imagePath,
+      })
+
+      return productCreated
+    } catch (err) {
+      console.error('An error occurred while create product', err)
+      throw new Error('An error occurred while create product, try again.')
+    }
+  }
+}
