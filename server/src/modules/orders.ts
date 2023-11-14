ccba3b15b82daae07b5050fc4a56b6ebac5fc1a0
Italo <italodopeey@gmail.com>
2023-11-14 10:16:28 -0300

    chore: eslint disable line prettier

--- server/src/modules/orders.ts
+++ server/src/modules/orders.ts
@@ -2,4 +2,5 @@ export { cancelOrderController } from '../http/controllers/order/cancel-order'
 export { changeOrderStatusController } from '../http/controllers/order/change-order-status'
 export { createOrderController } from '../http/controllers/order/create-orders'
 export { listOrdersController } from '../http/controllers/order/list-orders'
+// eslint-disable-next-line prettier/prettier
 
