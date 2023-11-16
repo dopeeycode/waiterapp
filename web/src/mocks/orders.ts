db9130c150c9f69157fec4595d191e4b5fded77d
Italo <italodopeey@gmail.com>
2023-11-16 20:46:43 -0300

    feat: mock orders

---
+++ web/src/mocks/orders.ts
@@ -0,0 +1,29 @@
+import { Order } from '../@types/Order'
+
+export const orders: Order[] = [
+  {
+    _id: '6553714ffd32dbb7ea248ef9',
+    table: '123',
+    status: 'WAITING',
+    products: [
+      {
+        product: {
+          name: 'Coca-Cola',
+          imagePath: '1699964355436-coca-cola.png',
+          price: 6,
+        },
+        quantity: 3,
+        _id: '6553714ffd32dbb7ea248efa',
+      },
+      {
+        product: {
+          name: 'Coca-Cola',
+          imagePath: '1699964355436-coca-cola.png',
+          price: 6,
+        },
+        quantity: 3,
+        _id: '6553714ffd32dbb7ea248efa',
+      },
+    ],
+  },
+]
