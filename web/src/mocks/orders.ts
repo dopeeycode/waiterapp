7d5821ca455a457dbbd5ab87e4b33bcb31ab4ec2
Italo <italodopeey@gmail.com>
2023-11-17 00:29:10 -0300

    chore: adding more products to test in mocks

--- web/src/mocks/orders.ts
+++ web/src/mocks/orders.ts
@@ -3,13 +3,13 @@ import { Order } from '../@types/Order'
 export const orders: Order[] = [
   {
     _id: '6553714ffd32dbb7ea248ef9',
-    table: '123',
+    table: '12',
     status: 'WAITING',
     products: [
       {
         product: {
           name: 'Coca-Cola',
-          imagePath: '1699964355436-coca-cola.png',
+          imagePath: '1699964328351-coca-cola.png',
           price: 6,
         },
         quantity: 3,
@@ -17,12 +17,12 @@ export const orders: Order[] = [
       },
       {
         product: {
-          name: 'Coca-Cola',
-          imagePath: '1699964355436-coca-cola.png',
-          price: 6,
+          name: 'Pizza quatro queijos',
+          imagePath: '1699661009713-quatro-queijos.png',
+          price: 45,
         },
-        quantity: 3,
-        _id: '6553714ffd32dbb7ea248efa',
+        quantity: 4,
+        _id: '654ec4d1079cc953cbcf3272',
       },
     ],
   },
